a3a5175e9bd7f5687ca41c508c98172bce428873
mrslvs <sekerka.miroslav@outlook.com>
2021-02-19 21:22:56 +0100

    added function set_serached_vec

--- guess.m
+++ guess.m
@@ -2,4 +2,20 @@
 %guess 5-digit number
 %isid92654
 
-score_matrix = zeros(1,8)
+NUM_OF_DIGITS = 5;
+pop_size = 8;
+
+num_min = 0;
+num_max = 9;
+
+%set random number as searched number
+searched_number = set_searched_vec(NUM_OF_DIGITS, num_max, num_min);
+
+
+function searched = set_searched_vec(digits, smax, smin)
+    searched = zeros(1,digits);
+    
+    for i = 1:digits
+        searched(i) = round(rand() * (smax - smin + 1) + smin);
+    end
+end
