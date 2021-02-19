1a2dddbf53789255a1fe47a15f566427c03d92ac
mrslvs <sekerka.miroslav@outlook.com>
2021-02-19 23:27:20 +0100

    added evaluation functions

--- guess.m
+++ guess.m
@@ -6,16 +6,29 @@ NUM_OF_DIGITS = 5;
 pop_size = 8;
 
 num_min = 0;
-num_max = 9;
+num_max = 8; %+1
 
 %set random number as searched number
 searched_number = set_searched_vec(NUM_OF_DIGITS, num_max, num_min);
 
-score_matrix = zeros(1, pop_size);
-population_matrix = zeros(pop_size, NUM_OF_DIGITS);
-
 %create population
 generation_matrix = set_population(NUM_OF_DIGITS, num_max, num_min, pop_size);
+
+function score_matrix = evaluation(generation, searched, digits, pop_size)
+    for individual = 1:pop_size
+        score_matrix(individual) = evaluate(generation(individual), searched, digits);
+    end
+end
+
+function score = evaluate(individual, searched, digits)
+    score = 0;
+    
+    for gene = 1:digits
+        if individual(gene) == searched(gene)
+            score = score + 1;
+        end
+    end
+end
 
 function searched = set_searched_vec(digits, smax, smin)
 %returns random number which will be searched for
@@ -35,17 +48,18 @@ function pop_matrix = set_population(digits, smax, smin, pop_size)
     pop_matrix = zeros(pop_size, digits);
     
     for individ = 1:pop_size
-        for digit = 1:digits
-            pop_matrix(individ, digit) = get_random_digit(smax, smin);
+        for gene = 1:digits
+            pop_matrix(individ, gene) = get_random_digit(smax, smin);
             
-            if digit == 1
-                while pop_matrix(individ, digit) == 0
-                    pop_matrix(individ, digit) = get_random_digit(smax, smin);
+            %if we set the first digit for current individual
+            if gene == 1
+                %get new random number if the current equals to zero
+                while pop_matrix(individ, gene) == 0
+                    pop_matrix(individ, gene) = get_random_digit(smax, smin);
                 end
             end
         end
-    end    
-    
+    end
 end
 
 function digit = get_random_digit(smax, smin)
