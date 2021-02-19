13fa3b957936bbe3b09d78ae5d3ec6cd007d32ca
mrslvs <sekerka.miroslav@outlook.com>
2021-02-19 21:43:11 +0100

    added function get_random_digit

--- guess.m
+++ guess.m
@@ -11,11 +11,14 @@ num_max = 9;
 %set random number as searched number
 searched_number = set_searched_vec(NUM_OF_DIGITS, num_max, num_min);
 
-
 function searched = set_searched_vec(digits, smax, smin)
     searched = zeros(1,digits);
     
     for i = 1:digits
-        searched(i) = round(rand() * (smax - smin + 1) + smin);
+        searched(i) = get_random_digit(num_max, num_min);
     end
+end
+
+function digit = get_random_digit(smax, smin)
+    digit = round(rand() * (smax - smin + 1) + smin);
 end
